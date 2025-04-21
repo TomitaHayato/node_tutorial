059497b9d341ce351e8d530297a4cf0ce4aaa48f
hayato_52b <a99246780@icloud.com>
2025-04-21 18:45:07 +0900

    パス操作

--- 02_node_basic/030_write_file/start/example.js
+++ 02_node_basic/030_write_file/start/example.js
@@ -1,1 +1,16 @@
-console.log('hello, node.js');
+// ファイル操作のライブラリをimport
+const fs = require('fs');
+const path = require('path')
+
+console.log(__dirname);
+console.log(__filename);
+
+// 第一引数をカレントディレクトリとして、第二引数に相対パスを指定することで、その絶対パスを返す
+const distPath = path.resolve(__dirname, '../dist/text.txt');
+console.log(distPath);
+// join()は、Pathを連結させる。
+const pathJoin = path.join(__dirname, TextDecoderStream.txt);
+console.log(pathJoin);
+
+// const dirName = __dirname + '/test.txt'
+// fs.writeFileSync(distPath, 'hello, woooooooooorld');
