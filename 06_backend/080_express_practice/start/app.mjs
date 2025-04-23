40d326fa32d81681eaa871c4170584923087cbe1
hayato_52b <a99246780@icloud.com>
2025-04-23 22:32:56 +0900

    express1

--- 06_backend/080_express_practice/start/app.mjs
+++ 06_backend/080_express_practice/start/app.mjs
@@ -3,10 +3,16 @@ import express from 'express';
 const PORT = 8080;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
   res.send(`
     <h1>練習問題</h1>
-    <p>下記のフォームで送信したリクエストを「商品1、商品2がカートに追加されました。」のようなレスポンスとして返却する処理を記述しましょう</p>
+    <p>
+      下記のフォームで送信したリクエストを
+      「商品1、商品2がカートに追加されました。」
+      のようなレスポンスとして返却する処理を記述しましょう
+    </p>
     <form action="/cart" method="POST">
     <div>
       <label>商品：<input type="text" name="product[]"></label>
@@ -16,6 +22,17 @@ app.get('/', function (req, res) {
     </div>
     <input type="submit">
     </form>
-    `);
+  `);
 });
 
+app.post('/cart', (req, res) => {
+  const products = req.body.product;
+  const message = `${products[0]}、${products[1]}がカートに追加されました。`
+  res.send(`
+    <p>${message}</p>
+  `)
+});
+
+app.listen(PORT, () => {
+  console.log('サーバ起動');
+});
